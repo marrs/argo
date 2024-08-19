00a907457a3a84ee4508bb6b3cdbc5d9ee182ef9
David Marrs <david@marrsbytes.com>
2024-08-19 19:26:24 +0100

    Abandon html renderer and construct nodes directly
    Forgot that innerHTML won't know what to do with events!

--- argo.js
+++ argo.js
@@ -2,7 +2,7 @@ function type(x) {
     return Object.prototype.toString.call(x);
 }
 
-function html(ml) {
+function ml_to_nodes(ml) {
     if (!Array.isArray(ml)) {
         throw new Error("jsonML must be an array.")
     }
@@ -11,84 +11,105 @@ function html(ml) {
         return "";
     }
 
-    var target = [];
+    var nodes = [];
 
     var first = ml[0];
     var second = ml[1];
     var rest = ml.slice(2);
 
-    var tag = null;
-    var attr = '';
-
-    if (Array.isArray(first)) {
-        target.push(html(first));
-    } else if (typeof first === 'string') {
-        tag = '<' + first;
-    } else if (typeof first === 'function') {
-        target.push(html(first(second)));
-        second = null;
-        if (rest.length) {
-            throw new Error("Too many args", first);
-        }
-    } else {
-        throw new Error("Illegal element in first position");
+    function process_attrs(node, attrs) {
+        Object.keys(attrs).forEach(ky => {
+            if (type(attrs[ky]) === '[object Function]') {
+                node.addEventListener(ky, attrs[ky]);
+            } else {
+                node.setAttribute(ky, attrs[ky]);
+            }
+        });
     }
 
-    if (type(second) === '[object Object]') {
-        Object.keys(second).map((ky) => {
-            var vl = second[ky];
-            switch(type(vl)) {
-                case '[object String]': {
-                    attr = ky + '="' + vl + '"';
-                } break;
-                case '[object Boolean]': {
-                    if (vl) {
-                        attr = ky + '="' + ky + '"';
-                    }
-                } break;
-                case '[object Object]': {
-                    if ('style' === ky) {
-                        attr = ky + '="' + Object.keys(vl).map((ky) => ky + ':' + vl[ky]).join(';') + '"';
-                    }
-                }
+    switch (type(first)) {
+        case '[object Array]': {
+            nodes = nodes.concat(ml_to_nodes(first));
+        } break;
+        case '[object String]': {
+            var el = document.createElement(first);
+            if (type(second) === '[object Object]') {
+                process_attrs(el, second);
             }
-            return ky + '=' + '"';
-        });
-        tag += attr? ' ' + attr + '>' : '>';
-        target.push(tag);
-    } else {
-        tag && target.push(tag += '>');
-        if (Array.isArray(second)) {
-            target.push(html(second));
+            nodes.push(el);
+
+        } break;
+        case '[object Function]': {
+            nodes = ml_to_nodes(first(second));
+            if (rest.length) {
+                throw new Error("Too many args for component", first);
+            }
+        } return nodes;
+        default: {
+            throw new Error("Illegal element in first position");
+        }
+    }
+
+    if (second) {
+        if (type(second) === '[object Object]') {
+            process_attrs(nodes[nodes.length -1], second);
         } else {
-            second && target.push(second.toString());
+            rest.unshift(second);
         }
     }
 
     if (rest.length) {
-        for (var idx = 0, len = rest.length; idx < len; ++idx) {
-            if (Array.isArray(rest[idx])) {
-                target.push(html(rest[idx]));
-            } else {
-                target.push(rest[idx].toString()); // TODO: Validate
+        rest.forEach((item, idx) => {
+            switch (type(item)) {
+                case '[object Array]': {
+                    ml_to_nodes(item).forEach(node => {
+                        nodes.push(node);
+                    });
+                } break;
+                case '[object String]':
+                case '[object Boolean]':
+                case '[object Number]': {
+                    nodes[0].append(rest[idx].toString());
+                } break;
+                default: {
+                    throw new Error("Illegal type in position " + (nodes.length + idx));
+                }
             }
+        });
+    }
+    return nodes;
+}
+
+function argo(Component, el) {
+    var traps = {};
+
+    function fire(evt, payload) {
+        var handlers = traps[evt];
+        if (!handlers) {
+            console.warn("Event [", evt, "] not handled by Argo.");
+        } else {
+            handlers.forEach((handler) => {
+                handler(payload);
+            });
         }
     }
-    
-    if (rest.length
-    || second !== undefined
-    && second !== null
-    && type(second) !== '[object Object]'
-    && typeof first === 'string') {
-        target.push('</' + first + '>');
+
+    return {
+        render(props) {
+            var children = Array.prototype.slice.call(el.children);
+            children.forEach(child => {
+                el.removeChild(child);
+            });
+            children = undefined;
+            var jsonml = Component(fire).render(props);
+            el.append.apply(el, ml_to_nodes(jsonml));
+        },
+        on(evt, handler) {
+            var handlers = traps[evt] = traps[evt] || []
+            handlers.push(handler);
+        },
+        fire
     }
-    return target.join('');
 }
-
-var argo = {
-    html,
-}
-
-argo.html = html;
 
 window.argo = argo;
